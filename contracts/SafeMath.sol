85f5fc0c3aefd7372635d92dd91d053be853c4fb
Duarte Aragao <daragao@gmail.com>
2018-04-08 23:25:56 +0100

    WIP

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,25 +1,11 @@
-pragma solidity ^0.4.9;
+pragma solidity ^0.4.18;
 
-/* https://github.com/LykkeCity/EthereumApiDotNetCore/blob/master/src/ContractBuilder/contracts/token/SafeMath.sol */
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
 library SafeMath {
-  uint256 constant public MAX_UINT256 =
-    0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-
-  function safeAdd(uint256 x, uint256 y) pure internal returns (uint256 z) {
-    if (x > MAX_UINT256 - y) revert();
-    return x + y;
-  }
-
-  function safeSub(uint256 x, uint256 y) pure internal returns (uint256 z) {
-    if (x < y) revert();
-    return x - y;
-  }
-
-  function safeMul(uint256 x, uint256 y) pure internal returns (uint256 z) {
-    if (y == 0) return 0;
-    if (x > MAX_UINT256 / y) revert();
-    return x * y;
-  }
 
   /**
   * @dev Multiplies two numbers, throws on overflow.
@@ -38,13 +24,13 @@ library SafeMath {
   */
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
+    // uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
+    return a / b;
   }
 
   /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
   */
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
--- migrations/2_doart3.js
+++ migrations/2_doart3.js
@@ -1,6 +1,10 @@
 const doart3 = artifacts.require('Doart3Token');
+const Exchange = artifacts.require('Exchange');
+const HTLC = artifacts.require('HashedTimelock');
 
 module.exports = (deployer) => {
-  deployer.deploy(doart3);
+  deployer.deploy(doart3,'DRT','doart3 Token');
+  deployer.deploy(HTLC);
+  deployer.deploy(Exchange);
 };
 
