6af7cb4e6f43d066f74c6ad5117aca02db9d82b5
Duarte Aragao <daragao@gmail.com>
2018-04-09 18:32:11 +0100

    minor fixes to compile

--- contracts/ERC223_interface.sol
+++ contracts/ERC223_interface.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.11;
 
 contract ERC223Interface {
     uint public totalSupply;
-    function balanceOf(address who) constant returns (uint);
+    function balanceOf(address who) constant public returns (uint);
     function transfer(address to, uint value) public returns (bool);
     function transfer(address to, uint value, bytes data) public returns (bool);
     event Transfer(address indexed from, address indexed to, uint value, bytes data);
--- migrations/2_doart3.js
+++ migrations/2_doart3.js
@@ -1,9 +1,9 @@
 const doart3 = artifacts.require('Doart3Token');
-const Exchange = artifacts.require('Exchange');
+//const Exchange = artifacts.require('Exchange');
 const HTLC = artifacts.require('HashedTimelock');
 
 module.exports = (deployer) => {
   deployer.deploy(doart3,'DRT','doart3 Token');
   deployer.deploy(HTLC);
-  deployer.deploy(Exchange);
+ // deployer.deploy(Exchange);
 };
